c556c6127b116890e42b9597d09a0fc6e492de0c
geekhall <174821300@qq.com>
2023-01-26 12:34:22 +0800

    feat: init project

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <h1 class="home">Home</h1>
+  <hr />
+  <p>Home page</p>
+  <Counter />
+  <hr />
+</template>
+
+<script lang="ts" setup>
+import { computed, ref, reactive } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const cnt = computed(() => store.state.count)
+</script>
+
+<style lang="less" scoped>
+.home {
+  color: cyan;
+}
+</style>
